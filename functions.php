8cd3d8a3066d595ed5695af1a35d2512f4f362fc
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2019-02-09 23:30:55 +0100

    load parent's style guide

--- functions.php
+++ functions.php
@@ -1,3 +1,6 @@
 <?php
-
+    add_action( 'wp_enqueue_scripts', 'neerdpause_theme_enqueue_styles' );
+    function neerdpause_theme_enqueue_styles() {
+        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    }
 ?>
