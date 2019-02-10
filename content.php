b6e0cebf5da4cd532d6cf0225c67f45a0636790b
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2019-02-10 13:40:21 +0100

    replaced overridden base files with 2.8 version

--- content.php
+++ content.php
@@ -17,7 +17,12 @@
 		</div>
 		<?php endif; ?>
 		<header class="entry-header">
-			<?php the_post_thumbnail(); ?>
+			<?php
+			if ( ! post_password_required() && ! is_attachment() ) :
+				the_post_thumbnail();
+			endif;
+			?>
+
 			<?php if ( is_single() ) : ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 			<?php else : ?>
@@ -39,7 +44,14 @@
 		<?php else : ?>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+			<?php
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ),
+					'after'  => '</div>',
+				)
+			);
+			?>
 		</div><!-- .entry-content -->
 		<?php endif; ?>
 
