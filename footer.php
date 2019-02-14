cf49d1e9e7d3a42666c7a103e9a61b3a34970cd1
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2019-02-15 00:20:32 +0100

    use vertical dash as separator

--- footer.php
+++ footer.php
@@ -9,28 +9,26 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'twentytwelve_credits' ); ?>
-			Nerdpause
-			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( ' | ', '<span role="separator" aria-hidden="true"></span>' );
-			}
-			?>
-			<!--<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwelve' ) ); ?>" class="imprint" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>">
-				<?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?>
-			</a>-->
-			 | <a href="impressum" class="imprint">Impressum</a>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+      </div><!-- #main .wrapper -->
+            <footer id="colophon" role="contentinfo">
+                <div class="site-info">
+                    <?php do_action( 'twentytwelve_credits' ); ?>
+                    <a href="https://nerdpause.de" class="imprint" title="Nerdpause Blog">Nerdpause</a>
+                    <span role="separator" aria-hidden="true"></span>
+                    <?php
+                        if ( function_exists( 'the_privacy_policy_link' ) ) {
+                            the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+                        }
+                    ?>
+                    <a href="impressum" class="imprint">Impressum</a>
+                </div><!-- .site-info -->
+            </footer><!-- #colophon -->
+        </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 <!-- Place this tag after the last +1 button tag. -->
-<script type="text/javascript">
+        <script type="text/javascript">
   window.___gcfg = {lang: 'de'};
 
   (function() {
@@ -38,6 +36,6 @@
     po.src = 'https://apis.google.com/js/plusone.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
-</script>
-</body>
+        </script>
+    </body>
 </html>
