4ff951d39c2d003228b21707baea98ebad9b921c
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2019-02-15 21:28:08 +0100

    prepare for social media

--- footer.php
+++ footer.php
@@ -25,17 +25,7 @@
             </footer><!-- #colophon -->
         </div><!-- #page -->
 
-<?php wp_footer(); ?>
+        <?php wp_footer(); ?>
 
-<!-- Place this tag after the last +1 button tag. -->
-        <script type="text/javascript">
-  window.___gcfg = {lang: 'de'};
-
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-        </script>
     </body>
 </html>
