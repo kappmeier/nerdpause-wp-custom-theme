5dfd8faa90b8edb9f1d03ca5533ad5435a792bfc
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2019-02-15 21:28:28 +0100

    header cleanup

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * The Header template for our theme
+ * The Header template for our theme, taken from Twenty Twelve.
  *
- * Displays all of the <head> section and everything up till <div id="main">
+ * Displays all of the <head> section and everything up till <div id="main">.
  *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * - Update to Twenty Twelve original theme: Contains an android robot on the
+ *   right corner of the title bar.
+ * - Does not contain the top menu bar. Sites (Impressum, ...) are linked in the footer.
  */
 ?><!DOCTYPE html>
 <!--[if IE 7]>
@@ -19,63 +19,50 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width" />
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+    <!--[if lt IE 9]>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<header id="masthead" class="site-header" role="banner">
+    <div id="page" class="hfeed site">
+        <header id="masthead" class="site-header" role="banner">
 
-<!-- The left part of the header, containing blog title and subtitle. -->
-<div style="float:left;width:60%;">
-		<hgroup>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
-</div>
+            <!-- The left part of the header, containing blog title and subtitle. -->
+            <div style="float:left;width:60%;">
+                <hgroup>
+                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+                </hgroup>
+            </div>
 
-<!-- The right part of the header, containing an android. -->
-<div style="float:right;width:39%;height:120px;position:relative;">
-  <div class="droid">
-    <div class="head">
-      <div class="eye"></div>
-      <div class="eye"></div>
-      <div class="ear"></div>
-      <div class="ear"></div>
-    </div>
-    <div class="torso"></div>      
-    <div class="leg"></div>
-    <div class="leg"></div>
-    <div class="arm1"></div>
-    <div class="arm2"></div>
-  </div>
-</div>
+            <!-- The right part of the header, containing an android. -->
+            <div style="float:right;width:39%;height:120px;position:relative;">
+                <div class="droid">
+                    <div class="head">
+                        <div class="eye"></div>
+                        <div class="eye"></div>
+                        <div class="ear"></div>
+                        <div class="ear"></div>
+                    </div>
+                    <div class="torso"></div>            
+                    <div class="leg"></div>
+                    <div class="leg"></div>
+                    <div class="arm1"></div>
+                    <div class="arm2"></div>
+                </div>
+            </div>
 
-		<!--<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'primary',
-					'menu_class'     => 'nav-menu',
-				)
-			);
-			?>
-		</nav>--><!-- #site-navigation -->
+        <?php if ( get_header_image() ) : ?>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
+        <?php endif; ?>
+    </header><!-- #masthead -->
 
-		<?php if ( get_header_image() ) : ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
-		<?php endif; ?>
-	</header><!-- #masthead -->
-
-	<div id="main" class="wrapper">
+    <div id="main" class="wrapper">
